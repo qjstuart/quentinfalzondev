051bc7811a3c7ac7171cd6250e9d2483b137f4c2
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-01-17 21:54:15 +0100

    set apple music player theme to auto instead of re-rendering to match website theme

--- src/components/AppleMusicPlayer.tsx
+++ src/components/AppleMusicPlayer.tsx
@@ -22,7 +22,7 @@ export default function AppleMusicPlayer({ appleMusicId }: { appleMusicId: strin
         className="rounded-[15px]"
         width="100%"
         height="450"
-        src={`https://embed.music.apple.com/us/album/${appleMusicId}?theme=${theme}`}
+        src={`https://embed.music.apple.com/us/album/${appleMusicId}?theme=auto`}
         allow="encrypted-media; fullscreen; clipboard-write;"
         sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
         title="Media player"
