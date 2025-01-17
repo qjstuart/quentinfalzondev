76c0786d63b8238d1cb63a10c570eaf8e266b3c7
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-01-17 22:00:41 +0100

    resolve eslint build errors

--- src/components/AppleMusicPlayer.tsx
+++ src/components/AppleMusicPlayer.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { useTheme } from "next-themes"
+// import { useTheme } from "next-themes"
 import { useState, useEffect } from "react"
 
 export default function AppleMusicPlayer({ appleMusicId }: { appleMusicId: string }) {
   const [mounted, setMounted] = useState(false)
-  const { theme } = useTheme()
+  // const { theme } = useTheme()
 
   // We need to wait until it is safe to show the UI.
   // Otherwise we risk hydration mismatch errors.
