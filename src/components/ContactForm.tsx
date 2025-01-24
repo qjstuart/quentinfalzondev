38efdc43c12f27e3f28320c46e595f364724d8ae
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-01-24 16:23:05 +0100

    troubleshoot contact form submission

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -23,7 +23,7 @@ export default function ContactForm() {
       // body: new URLSearchParams(formData).toString(),
     }
 
-    const response = await fetch("/arrow.svg", requestOptions)
+    const response = await fetch("/", requestOptions)
     console.log("response: ", response)
 
     if (response.ok) {
