ec66aaf16c3780469ab5680a1356180e5ebe56d0
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-01-27 23:06:09 +0100

    amend footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ export default function Footer({ classes }: { classes: string }) {
       <footer className={`${classes} flex justify-center pb-8`}>
         <div className="text-gray">
           <em>&#169; </em>
-          <em>{currentYear} Quentin Falzon. </em>
-          <em>All Rights Reserved.</em>
+          <em>{currentYear} Quentin Falzon</em>
+          {/* <em>All Rights Reserved.</em> */}
         </div>
       </footer>
     </>
