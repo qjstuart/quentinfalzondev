dd298fe916af729727efceb4a08ae3ddcdb65bfd
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-02-02 12:48:01 +0100

    change project card design to single-column & add secondary description placeholders

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,27 +8,27 @@ type ProjectCardProps = {
   href: string
 }
 
-export default function ProjectCard({
-  description,
-  iconSrc,
-  href,
-}: ProjectCardProps) {
+export default function ProjectCard({ description, iconSrc, href }: ProjectCardProps) {
   return (
     <Link
       href={href}
-      className="flex justify-center items-center size-[220px] md:size-[280px] rounded-lg shadow-projectcard bg-project-card-bg hover:bg-project-card-bg-hover hover:cursor-pointer transition-colors"
+      className="flex justify-center items-center px-4 sm:px-12 py-6 gap-4 sm:gap-8 rounded-lg shadow-projectcard bg-project-card-bg hover:bg-project-card-bg-hover hover:cursor-pointer transition-colors"
     >
-      <div className="grid grid-rows-[1fr_auto] justify-items-center gap-5">
-        <div className="size-[100px]  md:size-[150px] relative">
-          <Image
-            fill
-            sizes="(max-width: 640px) 100px, 150px"
-            src={iconSrc}
-            alt={description}
-          />
-        </div>
-        <p className="text-xl md:text-2xl">{description}</p>
+      {/* <div className="grid grid-rows-[1fr_auto] justify-items-center gap-5"> */}
+      <div className="size-[60px]  md:size-[80px] relative">
+        <Image
+          fill
+          sizes="(max-width: 640px) 100px, 150px"
+          src={iconSrc}
+          alt={description}
+          className="object-contain"
+        />
       </div>
+      <div className="flex flex-col">
+        <p className="text-lg sm:text-2xl">{description}</p>
+        <p className="text-md text-gray">Test secondary description</p>
+      </div>
+      {/* </div> */}
     </Link>
   )
 }
