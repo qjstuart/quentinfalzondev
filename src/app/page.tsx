059c6b110a5c62ff34929fdc714ec90539bd9438
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-02-04 18:43:06 +0100

    homepage layout wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Button from "@/components/Button"
+
 export default function Home() {
   return (
     <div className="max-w-[1200px] mx-auto">
@@ -6,12 +8,29 @@ export default function Home() {
           <h1 className="text-[40px] md:text-[45px] lg:text-[50px] tracking-tight leading-tight text-nowrap">
             Hi there! <br /> I&apos;m{" "}
             <span className="font-semibold font-secondary italic pb-3 inline-block">
-              Quentin Falzon
+              Quentin Falzon.
             </span>
           </h1>
         </div>
-        <div>
-          <h2 className="md:text-[25px] lg:text-[30px] leading-10">A Web Developer based in Valletta, Malta</h2>
+        <div className="">
+          <h2 className="md:text-[25px] lg:text-[30px] leading-8 lg:leading-10 pb-2">
+            A Web Developer based in Valletta, Malta.
+          </h2>
+          <p className="text-gray text-lg">
+            Passionate about crafting good experiences on the web.
+          </p>
+          <div className="flex justify-start gap-6 mt-10">
+            <Button
+              classes="bg-foreground text-background hover:bg-[rgba(var(--foreground),0.75)]"
+              text="View My Work"
+              href=""
+            />
+            <Button
+              classes="bg-background text-foreground border border-foreground hover:bg-[rgba(var(--foreground),0.15)]"
+              text="Get In Touch"
+              href=""
+            />
+          </div>
         </div>
       </section>
     </div>
