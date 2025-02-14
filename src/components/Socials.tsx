de23d1c7694e1e0405301b82591629f56fd05844
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-02-14 13:26:46 +0100

    pre-render socials images for faster loading

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import Image from "next/image"
 
 import { useTheme } from "next-themes"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useMemo } from "react"
 
 import { invertLogoColorBasedOnTheme } from "@/app/lib/utils"
 
@@ -25,6 +25,35 @@ export default function Socials() {
     }
   }, [theme, mounted])
 
+  // Pre-render the images and store them for later use
+  const githubImage = useMemo(
+    () => (
+      <Image
+        src="/logo-github.png"
+        alt="GitHub"
+        fill
+        priority
+        className="transition-opacity opacity-0 duration-[0.5s]"
+        onLoadingComplete={(image) => image.classList.remove("opacity-0")}
+      />
+    ),
+    []
+  )
+  
+  const linkedInImage = useMemo(
+    () => (
+      <Image
+        src="/logo-linkedin.png"
+        alt="LinkedIn"
+        fill
+        priority
+        className="transition-opacity opacity-0 duration-[0.5s]"
+        onLoadingComplete={(image) => image.classList.remove("opacity-0")}
+      />
+    ),
+    []
+  )
+
   if (!mounted) {
     // Render placeholder to prevent Cumulative Layout Shift (CLS)
     return <div className="relative h-[48px] w-full"></div>
@@ -38,14 +67,14 @@ export default function Socials() {
         target="_blank"
         className="relative size-[30px] md:size-[40px]"
       >
-        <Image src="/logo-github.png" alt="GitHub" fill />
+        {githubImage}
       </Link>
       <Link
         href="https://www.linkedin.com/in/quentin-falzon-b7b56a19b/"
         target="_blank"
         className="relative size-[30px] md:size-[40px]"
       >
-        <Image src="/logo-linkedin.png" alt="LinkedIn" fill />
+        {linkedInImage}
       </Link>
     </div>
   )
