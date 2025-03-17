34a9fd6a4e1ead62cae8e8b07659fdbd5a36a30d
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-03-17 11:37:05 +0100

    add logo for this portfolio project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,31 +13,35 @@ export default function Projects() {
             title="Music Catalog Browser"
             description="Using the Discogs API to showcase my music collection."
             href={"/projects/record-collection/about"}
+            imgClass=""
           />
           <ProjectCard
             iconSrc="/globe.svg"
             title={`Daniel Sant's Website`}
             description={`The official website of pianist/composer Daniel Sant.`}
             href={"/projects/daniel-sant-website/about"}
+            imgClass=""
+          />
+          <ProjectCard
+            iconSrc="/nasa-logo.svg"
+            title="NASA Pic Of The Day"
+            description={`Discover the cosmos with NASA's APOD (Astronomy Picture of the Day) API!`}
+            href={"/projects/daniel-sant-website/about"}
+            imgClass=""
           />
           <ProjectCard
             iconSrc="/daily_word_detectives.png"
             title="Daily word detectives"
             description={`A not-so covert replica of the famous "Wordle" game.`}
             href={"/projects/daily-word-detectives/about"}
-          />
-
-          <ProjectCard
-            iconSrc="/nasa-logo.svg"
-            title="NASA Pic Of The Day"
-            description={`Discover the cosmos with NASA's APOD (Astronomy Picture of the Day) API!`}
-            href={"/projects/daniel-sant-website/about"}
+            imgClass=""
           />
           <ProjectCard
-            iconSrc="/logo-nextjs.svg"
+            iconSrc="/logo-portfolio.png"
             title="This Portfolio Website"
             description={`Built using NextJS, Tailwind and TypeScript.`}
             href={"/projects/daniel-sant-website/about"}
+            imgClass="rounded-xl opacity-90"
           />
         </ul>
       </div>
